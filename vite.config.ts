9a04c2498a21222a3fecc083ecaa76c39e1dffc2
Quy Nguyen <quynguyen@tomochain.com>
2023-08-26 22:49:45 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mintbase',
+  base: '/mintbase/',
   plugins: [react()],
   server: {
     port: 3000
